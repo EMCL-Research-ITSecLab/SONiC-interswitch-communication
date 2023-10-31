36ca930dc9853c4d26e32052613d3b542032db5d
Max Ludwig <maxludwig3@t-online.de>
2023-10-31 08:54:36 +0100

    Testing: Finish minimal connection test via python

--- tests/test_rosenpass_container.py
+++ tests/test_rosenpass_container.py
@@ -1,25 +1,27 @@
 import subprocess
 import docker
 import time
-import os 
+import os
+
 
 def setup_module(module):
     subprocess.run(["docker-compose", "up", "-d"], capture_output=True, text=True)
     # ensure that the containers are up and running
-    time.sleep(10)
+    time.sleep(25)
+
 
 def teardown_module(module):
     subprocess.run(["docker-compose", "stop"])
     subprocess.run(["docker", "network", "rm", "tests_rosenpass"])
     subprocess.run(["docker-compose", "rm", "-f"])
 
+
 def test_ping_from_client():
     client = docker.from_env()
     container_name = "tests-client-1"
-    command_to_run = ["ping6", "fe90::3%rosenpass0"]
+    command_to_run = ["ping6", "-c", "4", "fe90::3%rosenpass0"]
     response = client.containers.get(container_name).exec_run(command_to_run)
 
     # Print the result
-    print(response.output.decode("utf-8"))
-
-
+    print(f"Exit code: {response.exit_code}")
+    assert int(response.exit_code) == 0
