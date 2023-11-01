099808a9ee9a6befbd52f157b62013abefa24779
Max Ludwig <maxludwig3@t-online.de>
2023-11-01 10:56:28 +0100

    Refactoring: Rework test logging

--- tests/test_rosenpass_container.py
+++ tests/test_rosenpass_container.py
@@ -1,10 +1,18 @@
 import subprocess
 import docker
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
 def setup_module(module):
     # start the containers; Healthcheck in compose file ensures that the containers are ready
     subprocess.run(["docker-compose", "up", "--wait"], capture_output=True, text=True)
+    # Log if everything is setup correctly
+    ps = subprocess.run(["docker", "ps", "-a"], capture_output=True, text=True)
+    nw = subprocess.run(["docker", "network", "ls"], capture_output=True, text=True)
+    LOGGER.info(ps.stdout)
+    LOGGER.info(nw.stdout)
 
 
 def teardown_module(module):
@@ -19,6 +27,5 @@ def test_ping_from_client():
     command_to_run = ["ping6", "-c", "4", "fe90::3%rosenpass0"]
     response = client.containers.get(container_name).exec_run(command_to_run)
 
-    # Print the result
-    print(f"Exit code: {response.exit_code}")
+    LOGGER.info(f"Exit code: {response.exit_code}")
     assert int(response.exit_code) == 0
